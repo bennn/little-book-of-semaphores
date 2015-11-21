aeb14081722c9ca77850359fbfa73d0f3447cd3d
ben <types@ccs.neu.edu>
2015-11-21 18:36:32 -0500

    [3-4] nightclub simulator

---
+++ problems/3-4-multiplex.rkt
@@ -0,0 +1,20 @@
+#lang racket/base
+(require little-book-of-semaphores)
+
+(define CAPACITY 2)
+
+(define bouncer (make-semaphore CAPACITY))
+
+(define-syntax-rule (make-clubber name* ...)
+  (begin
+    (define-thread name*
+      (wait bouncer)
+      (printf "~a is in the club\n" (object-name name*))
+      (random-sleep)
+      (signal bouncer)
+      (printf "~a says goodbye\n" (object-name name*))) ...))
+
+(make-clubber
+  peter jasmine sarah susan greg scarlet erluo roman ben)
+
+(module+ main (run))
