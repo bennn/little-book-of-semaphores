79b0b52fb72993bfd777eed3e10d990712ecbb97
ben <types@ccs.neu.edu>
2015-11-21 19:35:22 -0500

    [3-3] using boxes, not set

--- problems/3-3-mutex.rkt
+++ problems/3-3-mutex.rkt
@@ -1,20 +1,20 @@
 #lang racket/base
 (require little-book-of-semaphores)
 
-(define count 0)
+(define count (box 0))
 
 (define mutex (make-semaphore 1))
 
 (define-thread A
   (wait mutex)
-  (set! count (+ count 1))
+  (incr count)
   (signal mutex))
 
 (define-thread B
   (wait mutex)
-  (set! count (+ count 1))
+  (incr count)
   (signal mutex))
 
 (module+ main
   (run)
-  (printf "Final count: ~a\n" count))
+  (printf "Final count: ~a\n" (unbox count)))
