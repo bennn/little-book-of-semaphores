3f638d8f21ce2660f9e1951ab54e23a7336095fb
ben <types@ccs.neu.edu>
2015-11-22 04:54:38 -0500

    [3-6] barrier and OO utility

---
+++ problems/3-6-reusable.rkt
@@ -0,0 +1,69 @@
+#lang racket/base
+(require little-book-of-semaphores)
+
+;; Resuable barrier
+
+(define *count-r* (box 0))
+(define *count-c* (box 0))
+
+(define can-enter? (make-semaphore 0))
+(define can-leave? (make-semaphore 1))
+(define mutex (make-semaphore 1))
+
+(define (decr/mutex b)
+  (wait mutex)
+  (decr b)
+  (define r (unbox b))
+  (signal mutex)
+  r)
+
+(define (incr/mutex b)
+  (wait mutex)
+  (incr b)
+  (define r (unbox b))
+  (signal mutex)
+  r)
+
+;; -----------------------------------------------------------------------------
+
+(define (rendezvous id)
+  (printf "~a : rendezvous\n" id)
+  (define o (decr/mutex *count-r*))
+  (if (zero? o)
+    (begin (wait can-leave?)
+           (signal can-enter?))
+    (begin (wait can-enter?)
+           (signal can-enter?)))
+  (incr/mutex *count-r*))
+
+(define (critical id)
+  (printf "~a : critical\n" id)
+  (define i (decr/mutex *count-c*))
+  (if (zero? i)
+    (begin (wait can-enter?)
+           (signal can-leave?))
+    (begin (wait can-leave?)
+           (signal can-leave?)))
+  (incr/mutex *count-c*))
+
+;; -----------------------------------------------------------------------------
+
+(define-syntax-rule (make-thread id)
+  (begin
+    (incr *count-r*)
+    (incr *count-c*)
+    (define-thread id
+      (rendezvous (object-name id))
+      (critical (object-name id)))))
+
+(make-thread A)
+(make-thread B)
+(make-thread C)
+(make-thread D)
+(make-thread E)
+
+(module+ main
+  (define NITERS 3)
+  (for ([_i (in-range NITERS)])
+    (run)))
+
---
+++ problems/barrier.rkt
@@ -0,0 +1,55 @@
+#lang racket/base
+(require little-book-of-semaphores)
+
+(require racket/class)
+
+(define barrier%
+  (class object%
+    (super-new)
+    (init-field N) ;; Natural
+    (field
+      [count N]
+      [mutex (make-semaphore 1)]
+      [can-enter? (make-semaphore 0)]
+      [can-leave? (make-semaphore 0)])
+
+    (define/public (barrier-wait)
+      (call-with-semaphore mutex
+        (lambda ()
+          (set-field! count this (sub1 count))
+          (when (zero? count)
+            (signal can-enter? N))))
+      (wait can-enter?))
+
+    (define/public (barrier-signal)
+      (call-with-semaphore mutex
+        (lambda ()
+          (set-field! count this (add1 count))
+          (when (= N count)
+            (signal can-leave? N))))
+      (wait can-leave?))))
+
+(provide barrier%)
+
+;; -----------------------------------------------------------------------------
+
+(module+ test
+  (define barrier (make-object barrier% 3))
+  (define-thread A
+    (printf "A waiting\n")
+    (send barrier barrier-wait)
+    (printf "A signalling\n")
+    (send barrier barrier-signal))
+  (define-thread B
+    (printf "B waiting\n")
+    (send barrier barrier-wait)
+    (printf "B signalling\n")
+    (send barrier barrier-signal))
+  (define-thread C
+    (printf "C waiting\n")
+    (send barrier barrier-wait)
+    (printf "C signalling\n")
+    (send barrier barrier-signal))
+  (for ([_i (in-range 5)])
+    (run))
+)
