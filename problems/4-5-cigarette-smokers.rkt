65b0ccfd3ff3b1f90c2567cc916d84a25314100f
ben <types@ccs.neu.edu>
2015-11-29 16:34:30 -0500

    [4-5] cleaner solution, at least less redundant

--- problems/4-5-cigarette-smokers.rkt
+++ problems/4-5-cigarette-smokers.rkt
@@ -5,89 +5,97 @@
 ;; - each smoker has one ingredient
 ;; - agent has all ingredients, makes 2 random ones available at a time
 
+(require (for-syntax racket/base syntax/parse racket/syntax))
+
 ;; -----------------------------------------------------------------------------
 ;; Agent
 
 (define agent-sem (make-semaphore 1))
-(define tobacco (make-semaphore 0))
-(define paper (make-semaphore 0))
 (define match (make-semaphore 0))
+(define paper (make-semaphore 0))
+(define tobacco (make-semaphore 0))
+(define ingredient* (vector match paper tobacco))
+
+(define-syntax-rule (ingredient-wait i)
+  (semaphore-wait (vector-ref ingredient* i)))
 
 (define-thread Controller
   (forever
     (sleep (random))
     (signal agent-sem)))
 
-(define-thread Agent-A
-  (forever
-    (wait agent-sem)
-    (printf "Make tobacco + paper\n")
-    (signal tobacco)
-    (signal paper)))
+(define-syntax make-agent
+  (syntax-parser
+   [(_ i1:id i2:id)
+    #:with name (gensym 'Agent)
+    #:with i1-sym (syntax-e #'i1)
+    #:with i2-sym (syntax-e #'i2)
+    #'(define-thread name
+        (forever
+          (wait agent-sem)
+          (printf "Making ~a and ~a\n" 'i1-sym 'i2-sym)
+          (signal i1)
+          (signal i2)))]))
 
-(define-thread Agent-B
-  (forever
-    (wait agent-sem)
-    (printf "Make match + paper\n")
-    (signal paper)
-    (signal match)))
-
-(define-thread Agent-C
-  (forever
-    (wait agent-sem)
-    (printf "Make match + tobacco\n")
-    (signal tobacco)
-    (signal match)))
+(make-agent tobacco paper)
+(make-agent paper match)
+(make-agent tobacco match)
 
 ;; -----------------------------------------------------------------------------
 
-(struct num (
-  matches
-  papers
-  tobacco
-))
-
 (define mutex (make-semaphore 1))
-(define match+paper (make-semaphore 0))
-(define match+tobacco (make-semaphore 0))
-(define tobacco+paper (make-semaphore 0))
+(define tobacco+match (make-semaphore 0))
+(define match+paper   (make-semaphore 0))
+(define paper+tobacco (make-semaphore 0))
 
-(define pouch (num (box 0) (box 0) (box 0)))
+;; Match Paper Tobacco
+(define pouch (vector 0 0 0))
+(define sema* (vector tobacco+match match+paper paper+tobacco))
 
-(define-syntax-rule (nonzero? field)
-  (not (zero? (unbox (field pouch)))))
+(define-syntax-rule (pouch-signal i)
+  (semaphore-post (vector-ref sema* i)))
 
-(define (try-signal i1 i2 s)
-  (when (and (nonzero? i1) (nonzero? i2))
-    (decr (i1 pouch))
-    (decr (i2 pouch))
-    (signal s)))
+(define-syntax-rule (pouch-wait i)
+  (semaphore-wait (vector-ref sema* i)))
 
-;; TODO cycle between signal orders
+(define-syntax-rule (pouch-incr i)
+  (vector-set! pouch i (add1 (vector-ref pouch i))))
 
-(define-thread Pusher-A
-  (forever
-    (wait match)
-    (with mutex
-      (incr (num-matches pouch))
-      (try-signal num-matches num-papers match+paper)
-      (try-signal num-matches num-tobacco match+tobacco))))
+(define-syntax-rule (pouch-decr i)
+  (vector-set! pouch i (sub1 (vector-ref pouch i))))
 
-(define-thread Pusher-B
-  (forever
-    (wait paper)
-    (with mutex
-      (incr (num-papers pouch))
-      (try-signal num-matches num-papers match+paper)
-      (try-signal num-papers num-tobacco tobacco+paper))))
+(define-syntax-rule (nonzero? i)
+  (not (zero? (vector-ref pouch i))))
 
-(define-thread Pusher-C
-  (forever
-    (wait tobacco)
-    (with mutex
-      (incr (num-matches pouch))
-      (try-signal num-tobacco num-papers tobacco+paper)
-      (try-signal num-matches num-tobacco match+tobacco))))
+(define (try-signal-both i #:phase b)
+  (define i+ (modulo (add1 i) 3))
+  (define i- (modulo (sub1 i) 3))
+  (cond [b    (try-signal i i+)
+              (try-signal i- i)]
+        [else (try-signal i- i)
+              (try-signal i i+)]))
+
+(define (try-signal lo hi)
+  (when (and (nonzero? lo) (nonzero? hi))
+    (pouch-decr lo)
+    (pouch-decr hi)
+    (pouch-signal hi)))
+
+(define-syntax make-pusher
+  (syntax-parser
+   [(_ ingredient:nat)
+    #:with name (format-id #'ingredient "Pusher-~a" (syntax-e #'ingredient))
+    #'(define-thread name
+        (let loop ([phase #f])
+          (ingredient-wait ingredient)
+          (with mutex
+            (pouch-incr ingredient)
+            (try-signal-both ingredient #:phase phase))
+          (loop (not phase))))]))
+
+(make-pusher 0)
+(make-pusher 1)
+(make-pusher 2)
 
 ;; -----------------------------------------------------------------------------
 
@@ -95,23 +103,18 @@
   (displayln msg)
   (sleep 1))
 
-;; Has matches
-(define-thread Smoker-A
-  (forever
-    (wait tobacco+paper)
-    (smoke "Smoker A is lit!")))
+(define-syntax make-smoker
+  (syntax-parser
+   [(_ i:nat)
+    #:with name (format-id #'i "Smoker-~a" (syntax-e #'i))
+    #`(define-thread name
+        (forever
+          (pouch-wait i)
+          (smoke (format "~a is lit!" '#,(syntax-e #'name)))))]))
 
-;; Has tobacco
-(define-thread Smoker-B
-  (forever
-    (wait match+paper)
-    (smoke "Smoker B is lit!")))
-
-;; Has paper
-(define-thread Smoker-C
-  (forever
-    (wait match+tobacco)
-    (smoke "Smoker C is lit!")))
+(make-smoker 0)
+(make-smoker 1)
+(make-smoker 2)
 
 ;; -----------------------------------------------------------------------------
 
